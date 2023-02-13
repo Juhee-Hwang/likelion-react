076acf5b1f25328929f6f4424c521fc2d79227c7
Juhee-Hwang <julia0276@naver.com>
2023-02-13 14:29:23 +0900

    [:sparkles: learn] createElement 함수 활용 코드

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,58 @@
 // nomodule
 import "./utils/supportJavaScript.js";
-import "./vanillaDOM.js";
+import "./vanilla.script.js";
 
-console.log("React 개발 시작하기! 😊");
+// console.log('React 개발 시작하기! 😊');
+
+// 명령형 프로그래밍
+// const root = document.getElementById("root");
+// const headline = root.querySelector("hi");
+
+// document.addEventListener("click", () => {
+// headeline.textContent = "update headline content";
+// });
+
+// React가 제공
+// 선언형 프로그래밍
+// import { state, render, update } from "./vanilla.script.js";
+
+// render(state);
+
+// document.addEventListener("click", () => {
+//   update({
+//     subjects: "React는 선언형 프로그래밍을 추구합니다.",
+//   });
+//   console.log(state);
+// });
+
+
+import { createElement, state, render, update } from './vanilla.script.js';
+
+import { createElement, state, render, update } from './vanilla.script.js';
+const app = createElement(
+  'section', 
+  {
+    'class': 'app',
+    'aria-label': 'Like React App'
+  }, 
+  createElement('h1', {}, 'hello App')
+  createElement('h1', {}, 'hello App'),
+  createElement('p', {}, 'blab blab blab'),
+  createElement('ul', {}, 
+    createElement('li', {}, 'item 1'),
+    createElement('li', {}, 'item 2'),
+    createElement('li', {}, 'item 3'),
+  ),
+);
+
+console.log(app);
+document.getElementById('root').append(app);
+
+document.addEventListener('click', () => {
+  update({
+    subjects: 'React는 선언형 프로그래밍 방식을 사용합니다.'
+  });
+
+  // console.log(state);
+});
+
